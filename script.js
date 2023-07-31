272c7f1b203534c58a3a2af4731f942d7b3e0daa
marco <mccarrera00@gmail.com>
2023-07-31 10:30:39 +0200

    Rename to shapes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function go() {
     var chords = ["A", "A#", "Ab", "B", "B#", "Bb", "C", "C#", "Cb", "D", "D#", "Db", "E", "E#", "Eb", "F", "Fb", "F#", "G", "G#", "Gb"];
-    var forms = ["C", "A", "G", "E", "D"];
+    var shapes = ["C", "A", "G", "E", "D"];
     var chord = chords[Math.floor(Math.random() * chords.length)];
-    var form = forms[Math.floor(Math.random() * forms.length)];
+    var shape = shapes[Math.floor(Math.random() * shapes.length)];
     document.getElementById("chord").textContent = "Chord " + chord;
-    document.getElementById("form").textContent = "Form " + form;
+    document.getElementById("shape").textContent = "Shape " + shape;
 }
